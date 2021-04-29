fd2b13131fad7e62ce90c09edfc2654586eb5077
sushant2308 <sushantr.ug18.ee@nitp.ac.in>
2021-04-29 13:44:42 +0530

    first commit

---
+++ Math/Exponentiation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 1001;
+const int MOD = 1000000007;
+const ll MAX = 1e9;
+int modpow(int a,int b, int m){
+    if(b==0) return 1%m;
+    long long u= modpow(a,b/2,m);
+    u= (u*u)%m;
+    if(b%2==1) u=(u*a)%m;
+    return u;
+}
+void solve(){
+    int n,k;cin >>  n >> k;
+    cout << modpow(n,k,MOD)<< endl;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;cin >> t;
+    while(t--) solve();
+}
